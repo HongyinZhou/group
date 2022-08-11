9742e3a5682798de73e7d00dcf80e9543f63bfc8
HongyinZhou <42049807+HongyinZhou@users.noreply.github.com>
2022-08-11 17:02:56 +0800

    Update main.m
    v2.0
    this project can be used to design metalens

--- main.m
+++ main.m
@@ -4,10 +4,10 @@ global x_array
 global y_array
 global shape_ele
 global lambda_ele
-global mesh 
 global h_element
 global space
 global Nsteps
+global lambda
 %%
 %   the size of metasurface
 %                     x_array
@@ -35,13 +35,11 @@ global Nsteps
 %                 -----------
 %                   list4
 %%
-x_array = 10;
-y_array = 1;
+x_array = 9;
+y_array = 9;
 %select current folder(the root directory of this program)
 selpath = uigetdir;
 % parameters set
-% element's shape is a cylinder(1) or cuboid(0)
-shape_ele = 1;
 h_element = '800[nm]';%The unit must be enclosed in square brackets!(only support m/ mm/ um/ nm)
 % si substrate's thickness
 h_substrate = '0.3[um]';%The unit must be enclosed in square brackets!!!!!
@@ -67,7 +65,7 @@ lambda = str2data(lambda);
 
 % init elements
 param_design(1:x_array * y_array) = lambda_ele*0.5;
-% param_design=importdata('para.txt');
+% param_design=importdata('param.txt');
 % load('param');
 % param_design=radius_mask';
 % param_design = rand (1, x_array * y_array) * lambda_ele * 0.8;
@@ -75,22 +73,11 @@ param_design(param_design <= 100e-9) = 100e-9;
 param_design(param_design >= lambda_ele) = lambda_ele*0.95; 
 %--------------------------------------------------------------------------
 
-type_beam = 'plane';
-if strcmp(type_beam,'gaussian')
-	%define waist radius
-	w0 = 0.5e-6;
-	%define distance from waist
-	distance = -5e-6;
-end
 %flag for forward(0) and backward simulation(1)
 flag = 0;
 
-%desired electric field distribution
-phi = 0;%azimuth angle(degrees)
-theta = 10;%vertical angle(degrees)
-tolerance = 6;%optional
-
-% [list1, list2, list3, list4] = generator_boundary_position(param_design);
+%generate adjoint field(metalens)
+gene_E;
 
 Nsteps=500;
 step_size= 2.5e-2;
@@ -98,6 +85,7 @@ balance = 1;%optional
 %adam parameter(default)
 mopt = zeros(1,x_array * y_array);
 vopt = zeros(1,x_array * y_array);
+
 %if the folder isn't exist, creat
 check_folder('electrc_field_profile_flag_0', ...
              'electrc_field_profile_flag_1', 'FOM');
@@ -109,27 +97,15 @@ if exist('para_data.txt', 'file')
 end
 %%
 %%please fold this section,,,,it is not elegant
-%import FDTD --------------get forward and backward field------------------
-code1 = code_gene('source', 'ExAmp', 'FOM', 'backward_Ex_a');
-code2 = code_gene('source', 'ExPhs', 'FOM', 'backward_Ex_p');
-code3 = code_gene('source', 'EyAmp', 'FOM', 'backward_Ey_a');
-code4 = code_gene('source', 'EyPhs', 'FOM', 'backward_Ey_p');
-code5 = code_gene('source', 'EzAmp', 'FOM', 'backward_Ez_a');
-code6 = code_gene('source', 'EzPhs', 'FOM', 'backward_Ez_p');
-code_back = [code1, code2, code3, code4, code5, code6];
 code1 = code_gene('forward_field', 'Ex', 'FOM', 'forward_Ex');
 code2 = code_gene('forward_field', 'Ey', 'FOM', 'forward_Ey');
 code3 = code_gene('forward_field', 'Ez', 'FOM', 'forward_Ez');
-code_for = [code1, code2, code3];
+code4 = code_gene('forward_field', 'x', 'FOM', 'forward_x');
+code5 = code_gene('forward_field', 'y', 'FOM', 'forward_y');
+code_for = [code1, code2, code3, code4, code5];
 %--------------------------------------------------------------------------
 mesh_accuracy = 3;
 mesh_accuracy_meta = 0.02e-6;
-FOM_pre = 0;
-param_pre = [];
-gradient_pre = [];
-step_size_record = [];
-multip_factor = 1;
-symbol = 1;
 %%
 for i = 1:Nsteps
     %%%flag forward or backward simulation
@@ -142,14 +118,10 @@ for i = 1:Nsteps
         mesh_accuracy_meta = 0.01e-6;
     end
     parameter_name = ['all_parameters','.mat'];
-    save(parameter_name, 'flag', 'mesh', 'h_air', 'h_element', ...
-        'h_substrate','index_nano','lambda', 'lambda_ele',  'code_for',...
-        'selpath', 'Nsteps', 'param_design', 'phi', 'theta', 'shape_ele',...
-        'shape_ele', 'space', 'tolerance', 'type_beam', 'x_array',...
-        'y_array', 'i', 'mesh_accuracy', 'mesh_accuracy_meta');
-    if strcmp(type_beam,'gaussian')
-        save(parameter_name,'w0','distance','-append');
-    end
+    save(parameter_name, 'flag', 'h_air', 'h_element', 'h_substrate',...
+    'index_nano','lambda', 'lambda_ele',  'code_for', 'selpath',...
+    'Nsteps', 'param_design', 'space', 'x_array', 'y_array', 'i',...
+    'mesh_accuracy', 'mesh_accuracy_meta');
     surface_fdtd = appopen('fdtd');
     code=strcat('cd("', selpath, '");');
     appevalscript(surface_fdtd, code);
@@ -169,15 +141,31 @@ for i = 1:Nsteps
     filename = 'FOM\forward_Ez.mat';
     load(filename);
     Ez_for = temp;
-    %----------------------------------------------------------------------
     
+    filename = 'FOM\forward_x.mat';
+    load(filename);
+    X_for = temp;
+
+    filename = 'FOM\forward_y.mat';
+    load(filename);
+    Y_for = temp;
+    %----------------------------------------------------------------------
+    %get backward field----------------------------------------------------
+    [X_formesh,Y_formesh]=meshgrid(X_for,Y_for);
+    back_Ex = interp2(X, Y, E_x, X_formesh', Y_formesh', 'nearest');
+    back_Ey = interp2(X, Y, E_y, X_formesh', Y_formesh', 'nearest');
+    back_Ez = zeros(length(Y_for),length(X_for));
+    
+    FOM = conj(back_Ex) .* Ex_for + conj(back_Ey) .* Ey_for ...
+    + conj(back_Ez) .* Ez_for;
+    FOM_total = abs(sum(FOM(:)));
+    disp(['epoches:',num2str(i)]);
+    disp(['FOM: ',num2str(FOM_total)]);
     %backward simulation---------------------------------------------------
     flag = 1;
-    save(parameter_name, 'flag', 'mesh', 'h_air', 'h_element', ...
-        'h_substrate','index_nano', 'lambda', 'lambda_ele', 'code_back',...
-    'Nsteps', 'param_design', 'phi', 'theta', 'shape_ele', 'shape_ele'...
-    , 'space', 'tolerance', 'type_beam', 'x_array', 'y_array', 'i',...
-    'selpath', 'mesh_accuracy','mesh_accuracy_meta');
+    save(parameter_name, 'flag', 'h_air', 'h_element','h_substrate',...
+    'index_nano', 'lambda', 'lambda_ele','Nsteps', 'param_design',...
+    'space', 'x_array', 'y_array', 'i','selpath', 'mesh_accuracy','mesh_accuracy_meta');
     if strcmp(type_beam,'gaussian')
         save(parameter_name,'w0','distance','-append');
     end
@@ -186,73 +174,7 @@ for i = 1:Nsteps
     appevalscript(surface_fdtd, code);
     appclose(surface_fdtd);
     
-    %get backward field and calculate FOM----------------------------------
-    filename = 'FOM\backward_Ex_a.mat';
-    load(filename);
-    ExAmp = temp;
-    filename = 'FOM\backward_Ex_p.mat';
-    load(filename);
-    ExPhs = temp;
-    Ex_back = ExAmp .* (cos(ExPhs) + 1i .* sin(ExPhs));
-
-    filename = 'FOM\backward_Ey_a.mat';
-    load(filename);
-    EyAmp = temp;
-    filename = 'FOM\backward_Ey_p.mat';
-    load(filename);
-    EyPhs = temp;
-    Ey_back = EyAmp .* (cos(EyPhs) + 1i .* sin(EyPhs));
-
-    filename = 'FOM\backward_Ez_a.mat';
-    load(filename);
-    EzAmp = temp;
-    filename = 'FOM\backward_Ez_p.mat';
-    load(filename);
-    EzPhs = temp;
-    Ez_back = EzAmp .* (cos(EzPhs) + 1i .* sin(EzPhs));
-    
-    %normalization
-%     operator_back = (conj(Ex_back) .* Ex_back + conj(Ey_back) .* Ey_back ...
-%     + conj(Ez_back) .* Ez_back).^0.5;
-%     operator_for = (conj(Ex_for) .* Ex_for + conj(Ey_for) .* Ey_for ...
-%     + conj(Ez_for) .* Ez_for).^0.5;
-%     operator_back = max(operator_back(:));
-%     operator_for = max(operator_for(:));
-%     Ex_for = Ex_for / operator_for;
-%     Ey_for = Ey_for / operator_for;
-%     Ez_for = Ez_for / operator_for;
-%     Ex_back = Ex_back / operator_back;
-%     Ey_back = Ey_back / operator_back;
-%     Ez_back = Ez_back / operator_back;
-    FOM = conj(Ex_back) .* Ex_for + conj(Ey_back) .* Ey_for ...
-    + conj(Ez_back) .* Ez_for;
-    FOM_total = sum(FOM(:)) * conj(sum(FOM(:)));
-    %get backward field and calculate FOM----------------------------------
-%     diff = FOM_total - FOM_pre;
-%     if diff < 0
-%         if step_size > 0.1
-%             symbol = 1;
-%             multip_factor = 1;
-%         elseif step_size < -0.1
-%             symbol = -1;
-%             multip_factor = 1;
-%         else
-%         end
-%         step_size = step_size - 1e-4 * (multip_factor ^ 2) * symbol;
-%         disp(['step_size: ',num2str(step_size)]);
-%         param_design = param_pre + step_size * gradient_pre * lambda_ele;
-%         param_design(param_design <= 100e-9) = 100e-9;
-%         param_design(param_design >= lambda_ele) = lambda_ele*0.9;
-%         disp('adjust step_size ...');
-%         multip_factor = multip_factor + 1;
-%         continue;
-%     else
-%         step_size_record = [step_size_record,step_size];
-%         multip_factor = 1;
-%     end
-    disp(['epoches:',num2str(i)]);
-    disp(['FOM: ',num2str(FOM_total)]);
-%     FOM_pre = FOM_total;
+ 
     toc;
     gradient = 0;
     gra1 = [];
@@ -265,8 +187,6 @@ for i = 1:Nsteps
             filename = ['electrc_field_profile_flag_1\','Ez_list_adjoint_data',num2str(ii),'_',num2str(jj),'.mat'];
             load(filename);
             backward = C_x0/balance;
-%             disp('relat_elec');
-%             disp([num2str(max(abs(forward(:)))),'++++++', num2str(max(abs(backward(:))))]);
             gradient_ele = forward .* backward;
             gradient = gradient + sum(gradient_ele(:));
         end
@@ -283,8 +203,6 @@ for i = 1:Nsteps
             filename = ['electrc_field_profile_flag_1\','D_list_adjoint_data',num2str(ii),'_',num2str(jj),'.mat'];
             load(filename);
             backward = C_x0/balance;
-%             disp('relat_13_D');
-%             disp([num2str(max(abs(forward(:)))),'++++++', num2str(max(abs(backward(:))))]);
             gradient_ele = forward .* backward;
             gradient1 = gradient1 + sum(gradient_ele(:));
         end
@@ -295,29 +213,21 @@ for i = 1:Nsteps
             filename = ['electrc_field_profile_flag_1\','D_list_adjoint_data',num2str(ii),'_',num2str(jj),'.mat'];
             load(filename);
             backward = C_x0/balance;
-%             disp('relat_24_D');
-%             disp([num2str(max(abs(forward(:)))),'++++++', num2str(max(abs(backward(:))))]);
             gradient_ele = forward .* backward;
             gradient2 = gradient2 + sum(gradient_ele(:));
         end
         gra2(ii) = 0.5*1i*lambda*(index_nano^2-1.444^2)*(gradient1 + gradient2);
     end
-    F = conj(Ex_back) .* Ex_for + conj(Ey_back) .* Ey_for ...
-    + conj(Ez_back) .* Ez_for;
+    F = conj(back_Ex) .* Ex_for + conj(back_Ey) .* Ey_for ...
+    + conj(back_Ez) .* Ez_for;
     gradient_origin = 2 * real (conj(sum(F(:))) * (gra1 + gra2));
-%     grad_max = max(abs(gradient_origin));
-%     grad_norm = gradient_origin / grad_max;
+
     [grad_adam, mopt, vopt] = step_adam(gradient_origin, mopt, vopt, i);
-%     GRADIENT = grad_adam .* grad_max;
-%     param_design = param_design + step_size * grad_adam * lambda_ele * ...
-%         (0.5 * exp(-i/20) + (1 / i ^ 2) * 0.5);%attenuation factor
-%     param_pre = param_design;
-%     gradient_pre = grad_adam;
-    param_design = param_design - step_size * grad_adam * lambda_ele/(i ^ 0.5);
+    param_design = param_design - step_size * grad_adam * lambda_ele/(i^0.5);
     %enforce constraints
     param_design(param_design <= 100e-9) = 100e-9;
     param_design(param_design >= lambda_ele) = lambda_ele*0.9;
-%     para_data = [para_data;param_design];
+
     
     %write iteration data
     fid_fom = fopen('FOM_list.txt', 'a');
@@ -329,5 +239,3 @@ for i = 1:Nsteps
     fclose(fid_para);
     %----------------------------------------------------------------------
 end
-filename='stepsize_list.txt';
-save(filename,'step_size_record','-ascii');
